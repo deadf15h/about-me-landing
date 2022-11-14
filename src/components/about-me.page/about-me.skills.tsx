973423de06e74c8c0464daa11736aacee81d0ffd
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-14 15:46:54 +0300

    skills added, need to fix

--- src/components/about-me.page/about-me.skills.tsx
+++ src/components/about-me.page/about-me.skills.tsx
@@ -1,6 +1,9 @@
 import iTypescriptLogo from "../../assets/img/typescript-logo.jpg";
 import iReactLogo from "../../assets/img/react-logo.jpg";
 import iSassLogo from "../../assets/img/sass-logo.jpg";
+import iGitLogo from "../../assets/img/git-logo.png";
+import iOopLogo from "../../assets/img/oop-logo.png";
+import iLinuxLogo from "../../assets/img/linux-logo.jpg";
 import AppearingContainer from "../appearing-container";
 import AboutMeSkillItem from "./about-me.skill-item";
 import "./about-me.skills.sass";
@@ -15,6 +18,7 @@ const AboutMeSkills = () => {
         <div className="about-me__subtitle skills__subtitle">My stack</div>
         <AppearingContainer time={1} offset={-300}>
           <div className="skills__box">
+            {/* //FIXME  */}
             <AboutMeSkillItem
               skillLogo={iTypescriptLogo}
               skillTitle="Typescript"
@@ -29,6 +33,21 @@ const AboutMeSkills = () => {
               skillLogo={iSassLogo}
               skillTitle="SASS"
               skillRate={4}
+            />
+            <AboutMeSkillItem
+              skillLogo={iGitLogo}
+              skillTitle="Git"
+              skillRate={3}
+            />
+            <AboutMeSkillItem
+              skillLogo={iLinuxLogo}
+              skillTitle="Linux"
+              skillRate={2}
+            />
+            <AboutMeSkillItem
+              skillLogo={iOopLogo}
+              skillTitle="OOP"
+              skillRate={3}
             />
           </div>
         </AppearingContainer>
