0679a5dbf5ffb50bf723d55366fa4205cb7e3d0b
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-15 13:49:58 +0300

    translations to description added

--- src/components/about-me.page/about-me.description.tsx
+++ src/components/about-me.page/about-me.description.tsx
@@ -1,18 +1,23 @@
+import { Trans, useTranslation } from "react-i18next";
 import AppearingContainer from "../appearing-container";
 import "./about-me.description.sass";
 
 const AboutMeDescription = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="about-me__container description" id="description">
-      <div className="about-me__title">About me</div>
+      <div className="about-me__title">{t("about me")}</div>
       <div className="about-me__subtitle">
         <AppearingContainer time={1} offset={-300}>
-          Frontend developer from Tver, Russia. <br />
-          <br />
-          I'm studying in TvSU on at the Faculty of Applied Mathematics and
-          Cybernetics.
-          <br />
-          <br />I love programming.❤️
+          <Trans i18nKey="description.content">
+            Frontend developer from Tver, Russia. <br />
+            <br />
+            I'm studying in TvSU on at the Faculty of Applied Mathematics and
+            Cybernetics.
+            <br />
+            <br />I love programming.❤️
+          </Trans>
         </AppearingContainer>
       </div>
     </div>
--- src/localisation/translations/en.ts
+++ src/localisation/translations/en.ts
@@ -8,6 +8,10 @@ const en = {
     name: "Nikolai<1></1>Morozov",
     about: "Frontend Developer<1></1>21 years old, Tver",
   },
+  description: {
+    content:
+      "Frontend developer from Tver, Russia.<1></1><2></2>I'm studying in TvSU on at the Faculty of Applied Mathematics and Cybernetics.<4></4><5></5>I love programming.❤️",
+  },
 };
 
 export default en;
