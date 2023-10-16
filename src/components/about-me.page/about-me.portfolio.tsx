62c16ac41189e7c5cbaf85de24037cce86449862
dalogsamurai <thelogsamurai@gmail.com>
2023-10-16 17:12:40 +0300

    feat(portfolio): projects add

--- src/components/about-me.page/about-me.portfolio.tsx
+++ src/components/about-me.page/about-me.portfolio.tsx
@@ -1,52 +1,45 @@
-import AppearingContainer from "../appearing-container";
 import iGagarinExample from "../../assets/img/portfolio-gagarin.png";
-import iJachteeExample from "../../assets/img/portfolio-jachtee.png";
 import iGagarinLogo from "../../assets/img/gagarin-logo.svg";
-import iJachteeLogo from "../../assets/img/jachtee-logo.svg";
+import iKopidooAppExample from "../../assets/img/kopidoo-app-portfolio.png";
+import iKopidooPublicExample from "../../assets/img/kopidoo-public-portfolio.png";
+import iKopidooJournalExample from "../../assets/img/kopidoo-journal-portfolio.png";
+import iKopidooLogo from "../../assets/img/kopidoo-logo.svg";
 import { useTranslation } from "react-i18next";
-import { useWidth } from "../../hooks/use-width";
-import "./about-me.portfolio.sass";
 import AboutMePortfolioItem from "./about-me.portfolio-item.component";
+import "./about-me.portfolio.sass";
 
 const AboutMePortfolio = () => {
-	const { t } = useTranslation();
-	const { isMobileWidth } = useWidth();
+  const { t } = useTranslation();
 
-	return (
-		<div className="about-me__container description" id="portfolio">
-			<div className="about-me__title">{t("portfolio")}</div>
-			<AboutMePortfolioItem
-				projectTitle="GAGARIN Launchpad"
-				projectExample={iGagarinExample}
-				projectLogo={iGagarinLogo}
-				projectLink="https://app.gagarin.world/"
-			/>
-			{/* <AppearingContainer time={1} offset={-300}>
-				<div className="portfolio__item">
-					<img
-						src={isMobileWidth ? iGagarinLogo : iGagarinExample}
-						alt=""
-						className="portfolio__img"
-					/>
-					<a href="https://app.gagarin.world/" target="_blank" rel="noreferrer">
-						<div className="portfolio__title">GAGARIN Launchpad</div>
-					</a>
-				</div>
-			</AppearingContainer>
-			<AppearingContainer time={1} offset={-300}>
-				<div className="portfolio__item">
-					<img
-						src={isMobileWidth ? iJachteeLogo : iJachteeExample}
-						alt=""
-						className="portfolio__img"
-					/>
-					<a href="http://jachtee.net/" target="_blank" rel="noreferrer">
-						<div className="portfolio__title">Jachtee</div>
-					</a>
-				</div>
-			</AppearingContainer> */}
-		</div>
-	);
+  return (
+    <div className="about-me__container description" id="portfolio">
+      <div className="about-me__title">{t("portfolio")}</div>
+      <AboutMePortfolioItem
+        projectTitle="GAGARIN Launchpad"
+        projectExample={iGagarinExample}
+        projectLogo={iGagarinLogo}
+        projectLink="https://app.gagarin.world/"
+      />
+      <AboutMePortfolioItem
+        projectTitle="Kopidoo App"
+        projectExample={iKopidooAppExample}
+        projectLogo={iKopidooLogo}
+        projectLink="https://app.kopidoo.ru/"
+      />
+      <AboutMePortfolioItem
+        projectTitle="Kopidoo Public Site"
+        projectExample={iKopidooPublicExample}
+        projectLogo={iKopidooLogo}
+        projectLink="https://kopidoo.ru/"
+      />
+      <AboutMePortfolioItem
+        projectTitle="Kopidoo Journal"
+        projectExample={iKopidooJournalExample}
+        projectLogo={iKopidooLogo}
+        projectLink="https://journal.kopidoo.ru/"
+      />
+    </div>
+  );
 };
 
 export default AboutMePortfolio;
